234d35a3cec874bdaa5fd7894c77b4604e366c12
amrr <amrr>
2026-03-14 14:12:22 +0200

    Add parallel Execution and change log file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ import core.DriverManager;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
+import org.apache.logging.log4j.ThreadContext;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,23 +16,27 @@ import static org.openqa.selenium.remote.CapabilityType.PLATFORM_NAME;
 
 public class BaseTest {
 
-    @Parameters({"platformName", "deviceName"})
+    @Parameters({"platformName", "deviceName", "udid", "systemPort"})
     @BeforeClass
-    public void setUp(String platformName, String deviceName) throws Exception {
+    public void setUp(String platformName, String deviceName, String udid, String systemPort) throws Exception {
+
+        ThreadContext.put("device", deviceName);
 
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(PLATFORM_NAME, platformName);
         caps.setCapability("appium:deviceName", deviceName);
+        caps.setCapability("appium:udid", udid);
+
         URL url = new URL(PropertyReader.getProperty("androidURL"));
         AppiumDriver driver;
 
         switch (platformName.toLowerCase()) {
             case "android":
                 caps.setCapability("appium:automationName", PropertyReader.getProperty("androidAutomationName"));
-                caps.setCapability("appium:udid", "emulator-5554");
                 caps.setCapability("appium:newCommandTimeout", 300);
                 caps.setCapability("appium:appWaitActivity", "com.swaglabsmobileapp.MainActivity");
                 caps.setCapability("appium:appWaitDuration", 40000);
+                caps.setCapability("appium:systemPort", Integer.parseInt(systemPort));
 
                 String appPathFromConfig = PropertyReader.getProperty("androidAppLocation");
                 appPathFromConfig = appPathFromConfig.replace("/", File.separator).replace("\\", File.separator);
@@ -64,5 +69,6 @@ public class BaseTest {
     @AfterClass
     public void tearDown() {
         DriverManager.quitDriver();
+        ThreadContext.clearAll();
     }
 }
